92f99462fb2881d45ede4ce1a4bd65f0f6af7c1e
System Administrator <root@linzihengdeMacBook-Air.local>
2015-06-06 11:37:57 +0800

    delete all

--- public/LogInit.py
+++
@@ -1,9 +0,0 @@
-#-*- coding: UTF-8 -*-
-import logging
-import logging.config
-
-
-#读取logging日志模块的配置文件
-logging.config.fileConfig(r"./public/Lt_logging.conf")
-
-LOG = logging.getLogger('handler_hander_users')
